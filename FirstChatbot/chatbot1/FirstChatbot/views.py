c690eacda0165984458d3611a662de03d35f01ee
praneethsattavaram <praneethsattavaram@gmail.com>
2024-02-28 19:39:14 +0530

    Changes to be committed: 	modified:   FirstChatbot/chatbot1/FirstChatbot/__pycache__/views.cpython-312.pyc 	modified:   FirstChatbot/chatbot1/FirstChatbot/views.py 	modified:   FirstChatbot/chatbot1/db.sqlite3

--- FirstChatbot/chatbot1/FirstChatbot/views.py
+++ FirstChatbot/chatbot1/FirstChatbot/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import openai
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model = "gpt-3.5-turbo-instruct",
+        prompt = message,
+        max_tokens = 150,
+        n=1,
+        stop = None,
+        temperature = 0.7,
+    )
+    answer = response.choice[0].text.strip()
+    return answer
 
 # Create your views here.
 def FirstChatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = "Hi this is my response"
+        response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
