75d93482359994b2cf2d884ab2e146ed5e29c900
praneethsattavaram <praneethsattavaram@gmail.com>
2024-02-28 21:37:35 +0530

    Changes to be committed: 	modified:   FirstChatbot/chatbot1/FirstChatbot/__pycache__/urls.cpython-312.pyc 	modified:   FirstChatbot/chatbot1/FirstChatbot/__pycache__/views.cpython-312.pyc 	modified:   FirstChatbot/chatbot1/FirstChatbot/urls.py 	modified:   FirstChatbot/chatbot1/FirstChatbot/views.py 	new file:   FirstChatbot/chatbot1/templates/login.html 	new file:   FirstChatbot/chatbot1/templates/register.html

--- FirstChatbot/chatbot1/FirstChatbot/urls.py
+++ FirstChatbot/chatbot1/FirstChatbot/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.FirstChatbot, name='chatbot')
+    path('',views.FirstChatbot, name='chatbot'),
+    path('login',views.login, name='login'),
+    path('register',views.register, name='register'),
+    path('logout',views.logout, name='logout'),
 ]
 
--- FirstChatbot/chatbot1/FirstChatbot/views.py
+++ FirstChatbot/chatbot1/FirstChatbot/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import openai
 
+from django.contrib import auth
+
 openai_api_key = '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'
 openai.api_key = openai_api_key
 
@@ -24,3 +26,24 @@ def FirstChatbot(request):
         response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        if password1 == password2:
+            pass
+        else:
+            error_message = 'Password dont match'
+            return render(request, 'register.html',{'error_message': error_message})
+    return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+
