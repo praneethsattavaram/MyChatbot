7cae7f81e464290b022e00b6ab1572df395e495c
PRANEETH REDDY <108972019+praneethsattavaram@users.noreply.github.com>
2024-03-26 23:55:21 +0530

    Update models.py

--- FirstChatbot/chatbot1/FirstChatbot/models.py
+++ FirstChatbot/chatbot1/FirstChatbot/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Chat(models.Model):
+  user = models.ForeignKey(User, on_delete = models.CASCADE)
+  models = models.TextField()
+  message = models.TextField()
+  create_at = models.DataTimeField(auto_now_add = True)
+
+  def __str__(self):
+    return f'{self.user.username} : {self.message}'
